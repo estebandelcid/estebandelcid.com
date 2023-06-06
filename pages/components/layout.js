011c7f197855b9b15da1a21cf0cad8e38b7df1c7
Esteban Del Cid <estebandelcid.07@gmail.com>
2023-06-06 16:55:48 -0600

    [change] meta tags

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -12,12 +12,14 @@ export default function Layout({ children, home }) {
     <div className="font-sans min-h-[900px] flex flex-col justify-around py-10">
       <Head>
         <link rel="icon" href="/edc.ico" />
-        <meta name="description" content="My profile" />
+        <meta name="robots" content="all" />
+        <meta name="description" content="Portfolio from Esteban Del Cid" />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="/public/images/background-mini.jpg"
+          // content={`https://og-image.vercel.app/${encodeURI(
+          //   siteTitle
+          // )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
