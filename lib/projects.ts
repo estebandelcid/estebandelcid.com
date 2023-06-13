3b4cbc1644602d05587b8a5671f761f9d817b6af
Esteban Del Cid <estebandelcid.07@gmail.com>
2023-06-13 16:36:55 -0600

    [add] lib social and projects

---
+++ lib/projects.ts
@@ -0,0 +1,43 @@
+
+const projects: TProject[] = [
+  {
+    slug: "lapsso",
+    bgImg: "/images/mesh-lapsso.png",
+    title: "Lapsso",
+    description: "The place to store it all.",
+    tech: "React, Next.js, Typescript, Node.js, Prisma, tRPC, Zustand, Tailwind, ProseMirror, Websockets, ReactQuery Cypress, CI/CD, Github Actions.",
+    projectUrl: "https://www.laps.so",
+    links: [],
+  },
+  {
+    slug: "petgram",
+    bgImg: "/images/mesh-petgram.png",
+    title: "Petgram",
+    description: "Proyect about photos of pets.",
+    tech: "React, Node.js, Cypress, React Apollo, GraphQL, Styled-components, Workbox.",
+    projectUrl: "https://petgram-edelcid.vercel.app/",
+    links: [
+      {
+        url: "https://github.com/estebandelcid/Proyect-Gram",
+        title: "Github (Frontend)",
+      },
+    ],
+  },
+  {
+    slug: "thestore",
+    bgImg: "/images/mesh-theStore.png",
+    title: "The Store",
+    description: "A bakery by Ruth Subirachs",
+    tech: "React, Strapi CMS, Cloudflare, ESLint, CSS",
+    projectUrl: "https://thestore.click/",
+    links: [
+      {
+        url: "https://github.com/estebandelcid/Confi-Merch",
+        title: "Github (Frontend)",
+      },
+    ],
+  },
+];
+
+export default projects
+
---
+++ lib/socialLinks.ts
@@ -0,0 +1,3 @@
+export const LinkedinUrl = "https://www.linkedin.com/in/estebandelcid/"
+export const GithubUrl = "https://github.com/estebandelcid"
+export const TwitterUrl = "https://twitter.com/estebandelcid"
