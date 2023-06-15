2c9674e439d6061c10aad3e20f3b39418ecee6d5
Esteban Del Cid <estebandelcid.07@gmail.com>
2023-06-15 14:24:01 -0600

    [fix] Shadow button

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -18,26 +18,8 @@ export default function ThemeButton () {
     const currentTheme = theme === 'system' ? systemTheme : theme;
   return (
     <label className="relative">
-      {/* className=" relative flex justify-center items-center w-[7rem] h-[2rem] space-x-2 " */}
-      {/* {currentTheme === "dark" ? (
-            <button
-              className="bg-black-700 hover:bg-black w-28 rounded-md border-purple-400 border-2 p-4"
-              onClick={() => setTheme("light")}
-            >
-              {""}
-              <BsFillSunFill/>
-            </button>
-          ) : (
-            <button
-              className="bg-gray-100 hover:bg-gray-500 w-28 rounded-md border-purple-400 border-2 p-4"
-              onClick={() => setTheme("dark")}
-            >
-              {""}
-              <BsFillMoonFill/>
-            </button>
-          )} */}
       <select
-        className="text-xl relative transition duration-300 bg-inherit outline-none w-[7rem] appearance-none cursor-pointer hover:text-[#e71b40] dark:hover:border-white rounded-[24px] p-3 [#E71B40] hover:shadow-[0px_0px_10px_1px_] hover:shadow-red-500/40 max-[920px]:shadow-[0px_0px_10px_1px] max-[920px]:shadow-neutral-500/50"
+        className="text-xl relative transition duration-300 bg-inherit outline-none w-[7rem] appearance-none cursor-pointer hover:text-[#e71b40] dark:hover:border-white rounded-[24px] p-3 [#E71B40] hover:shadow-[0px_0px_10px_1px_] hover:shadow-red-500/20 max-[920px]:shadow-[0px_0px_10px_0.5px] max-[920px]:shadow-neutral-500/20"
         value={theme}
         onChange={(e) => setTheme(e.target.value)}
       >
