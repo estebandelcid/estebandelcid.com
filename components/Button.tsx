ac87329b06b7b05a8af3cbe881d902cbfad248d3
Esteban Del Cid <estebandelcid.07@gmail.com>
2023-06-21 14:22:51 -0600

    [fix] Button space

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,9 +5,9 @@ export default function Button ({href, children }: {
     href: string
 }){
 return (
-<button className="w-auto h-fit min-h-[50px] flex items-center ml-8 ">
+<button className="w-auto h-fit min-h-[50px] flex items-center ml-8 min-[1445px]:mt-6 ">
   <Link
-    className="font-medium p-4 rounded-[50px] text-xl md:text-3xl max-[920px]:shadow-[0px_0px_10px_0.01px] max-[920px]:shadow-red-500/20 lg:text-[39px] text-[#E71B40]  hover:text-black dark:hover:text-white  hover:shadow-neutral-500/20 hover:shadow-[0px_0px_10px_1px] dark:hover:shadow-stone-500/20 dark:hover:shadow-[0px_0px_10px_1px] transition duration-300"
+    className="font-medium p-4 rounded-[50px] text-xl md:text-3xl max-[1600px]:shadow-[0px_0px_10px_0.01px] max-[920px]:shadow-red-500/20 lg:text-[39px] text-[#E71B40]  hover:text-black dark:hover:text-white  hover:shadow-neutral-500/20 hover:shadow-[0px_0px_10px_1px] dark:hover:shadow-stone-500/20 dark:hover:shadow-[0px_0px_10px_1px] transition duration-300"
     href={href}
   >
     {children}
