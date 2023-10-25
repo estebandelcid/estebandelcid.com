edad3bfe79ad1fdadcfe6cd13bd3cc32a6b6333f
Esteban Del Cid <estebandelcid.07@gmail.com>
2023-10-24 19:59:05 -0600

    [delete] Blog examples

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,6 @@ import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 import { AppProps } from 'next/app'; 
 
-// export default function App({Component, pageProps}){
-//     return (
-//       <ThemeProvider enableSystem={true} attribute='class'>
-//         <Component {...pageProps} />
-//       </ThemeProvider>
-//     );
-// }
-
 function App({Component, pageProps}: AppProps){
     return (
       <ThemeProvider enableSystem={true} attribute='class'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,18 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/Layout";
-import { getSortedPostsData } from "../lib/posts";
-import DateFormat from "../lib/date";
-import HeaderSection from "../components/HeaderSection";
 import ListOfCards from "../components/ListOfCards";
-import { GetStaticProps } from "next";
-import { ProjectCard } from "../components/ProjectCard";
-import projects from "../lib/projects";
-import Link from "next/link";
 
-
-// export const getStaticProps: GetStaticProps = async() => {
-//   const allPostsData = getSortedPostsData();
-//   return {
-//     props: {
-//       allPostsData,
-//     },
-//   };
-// }
-
-
-export default function Home({ allPostsData }: {allPostsData:{
-  date: string 
-  title: string 
-  id: string}[]
-}) {
+export default function Home() {
 
   return (
-    <Layout home>
+    <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
 
       <div className=" h-auto w-screen ">
-        <section className=" w-full h-full grid mb-8 items-center place-items-center text-[22px] ">
+        <section className=" w-full h-full grid mb-8 items-center place-items-center">
           <ListOfCards />
-           {/* Blog seccion  */}
-      {/* <section className="text-xl leading-5 pt-px">
-        <h2 className="text-xl leading-[1.4px] my-6 mx-0">Blog</h2>
-        <ul className=" list-none p-0 m-0">
-          {allPostsData.map(({ id, date, title }) => (
-            <li className="mt-0 mx-0 mb-5" key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className=" text-[#666]">
-                <p>{DateFormat(date)}</p>
-              </small>
-              <br />
-            </li>
-          ))}
-        </ul>
-      </section> */}
-
-         
         </section>
       </div>
     </Layout>
