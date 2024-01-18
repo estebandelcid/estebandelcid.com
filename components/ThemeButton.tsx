3ff96761c14c06aa1869afd9bc6e7e0cd71a7c9c
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-01-18 17:44:45 -0600

    [change] Hover color and shadow

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -19,18 +19,20 @@ export default function ThemeButton () {
   return (
     <label className="relative">
       <select
-        className="text-xl relative transition duration-300 bg-inherit outline-none w-[7rem] appearance-none cursor-pointer hover:text-[#e71b40] dark:hover:border-white rounded-3xl p-3 [#E71B40] hover:shadow-[0px_0px_10px_1px_]  hover:shadow-red-500/20 max-[920px]:shadow-[0px_0px_10px_0.5px] max-[920px]:shadow-neutral-500/20 dark:max-[920px]:shadow-neutral-500/40 dark:hover:shadow-red-500/20"
+        className="text-xl text-black/60 relative transition duration-300 bg-inherit outline-none w-[7rem] appearance-none cursor-pointer hover:text-black rounded-3xl p-[0.75rem_2.75rem_0.75rem_1.2rem] 
+        shadow-custom
+        dark:text-white/60 dark:hover:text-white dark:shadow-none"
         value={theme}
         onChange={(e) => setTheme(e.target.value)}
       >
-        <option value="system">
+        <option value="m">
           System
         </option>
         <option value="dark">Dark</option>
         <option value="light">Light</option>
       </select>
       <span className=" absolute left-[5.2rem] pointer-events-none flex items-center bottom-2.5 top-3">
-        <MdArrowDownward
+        <MdArrowDownward 
           fill="#E71B40"
           size="1.25rem"
         />
