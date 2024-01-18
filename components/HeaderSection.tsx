17d5c6645d2d0e21b71e194efd18a7a8eaba2b77
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-01-18 16:51:19 -0600

    [change] Background move to name, more line height, reduced the font size.

--- components/HeaderSection.tsx
+++ components/HeaderSection.tsx
@@ -1,19 +1,14 @@
 
 export default function HeaderSection(){
     return (
-      <section className=" mx-8 w-auto h-auto flex-col justify-center font-sans font-semibold mb-40">
-        <p className="text-center text-3xl md:text-5xl lg:text-6xl leading-none">
-          Hi, I´m Esteban,
+      <section className=" mx-8 w-auto h-auto flex-col justify-center font-sans font-semibold mb-40 ">
+        <p className="text-center leading-tight text-2xl md:text-3xl lg:text-4xl lg:leading-tight">
+          Hi, I'm  
+          <span className="text-transparent bg-clip-text bg-text animate-b-text-move"> Esteban</span>
           <br />
-          <span className="text-transparent bg-clip-text bg-text animate-b-text-move">
-          Front End 
-          </span>
-          <span className="text-transparent"> </span>
-          Developer,
+          Front End Developer,
           <br />
-          from México to the
-          <br />
-          World
+          from México to the World
         </p>
       </section>
     );
