a1384a01dea0da19036f3e814d0243cc921d1231
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-01-18 18:02:43 -0600

    [fix] Size of width and arrow position

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -19,19 +19,17 @@ export default function ThemeButton () {
   return (
     <label className="relative">
       <select
-        className="text-xl text-black/60 relative transition duration-300 bg-inherit outline-none w-[7rem] appearance-none cursor-pointer hover:text-black rounded-3xl p-[0.75rem_2.75rem_0.75rem_1.2rem] 
-        shadow-custom
+        className="text-xl text-black/60 relative transition duration-300 bg-inherit outline-none w-[9rem] appearance-none cursor-pointer hover:text-black rounded-3xl p-[0.75rem_2.75rem_0.75rem_1.5rem] 
+        hover:shadow-custom
         dark:text-white/60 dark:hover:text-white dark:shadow-none"
         value={theme}
         onChange={(e) => setTheme(e.target.value)}
       >
-        <option value="m">
-          System
-        </option>
+        <option value="system">System</option>
         <option value="dark">Dark</option>
         <option value="light">Light</option>
       </select>
-      <span className=" absolute left-[5.1rem] pointer-events-none flex items-center bottom-2.5 top-3">
+      <span className=" absolute left-[6.5rem] pointer-events-none flex items-center bottom-2.5 top-3">
         <MdArrowDownward 
           fill="#E71B40"
           size="1.25rem"
