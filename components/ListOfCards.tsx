3caf27da972f9454809787c90079782d860c5b5a
Esteban Del Cid <estebandelcid.07@gmail.com>
2025-01-27 20:19:31 -0600

    [fix] Grid in 2xl

--- components/ListOfCards.tsx
+++ components/ListOfCards.tsx
@@ -3,7 +3,7 @@ import projects from "../lib/projects";
 
 export default function ListOfCards(){
     return (
-      <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 2xl:gap-16">
+      <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 2xl:gap-16">
         {projects.map((project) => (
             <ProjectCard key={project.slug} {...project} />
         ))}
